7b503e8f6ecb215c61f822d1660039c060bb20ed
rahidhaque <ontirahid@yahoo.com>
2024-05-13 12:35:25 +0600

    Modal Created for updating

--- src/pages/MyQuery/MyQuery.jsx
+++ src/pages/MyQuery/MyQuery.jsx
@@ -2,9 +2,26 @@ import PropTypes from "prop-types";
 import { FaQuestion } from "react-icons/fa";
 
 const MyQuery = ({ query }) => {
-  const { brand, url, productName, queryTitle, currentDate } = query;
+  const {
+    _id,
+    brand,
+    url,
+    productName,
+    queryTitle,
+    currentDate,
+    boycottReason,
+  } = query;
+
+  const handleUpdateClick = (id) => {
+    document.getElementById(`update_modal_${id}`).showModal();
+  };
+
+  const handleUpdateQuery = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="w-full overflow-hidden rounded-lg shadow-lg bg-gray-900 text-gray-100">
+    <div className="w-full overflow-hidden rounded-lg shadow-lg bg-gray-900 text-gray-100 h-full">
       <div
         className="flex items-end justify-end h-44 p-4 bg-gray-500 bg-center bg-cover"
         style={{
@@ -39,7 +56,93 @@ const MyQuery = ({ query }) => {
           <button className="btn btn-info">View Details</button>
         </div>
         <div className="flex items-center space-x-1">
-          <button className="btn btn-accent">Update</button>
+          <button
+            className="btn btn-accent"
+            onClick={() => handleUpdateClick(_id)}
+          >
+            Update
+          </button>
+          <dialog id={`update_modal_${_id}`} className="modal">
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">
+                Update your query for {productName}
+              </h3>
+              <div className="flex flex-col w-full max-w-md p-12 space-y-4 text-center bg-gray-900 dark:bg-gray-50 text-gray-100 dark:text-gray-800">
+                <div className="flex items-center justify-center text-center dark:bg-gray-50 dark:text-gray-800">
+                  <div className="flex items-center justify-center text-center bg-gray-900 dark:bg-gray-50 text-gray-100 dark:text-gray-800">
+                    <form
+                      onSubmit={handleUpdateQuery}
+                      className="flex flex-col w-full max-w-lg p-12 rounded shadow-lg text-gray-100 dark:text-gray-800"
+                    >
+                      <label
+                        htmlFor="name"
+                        className="self-start text-xs font-semibold"
+                      >
+                        Product Name
+                      </label>
+                      <input
+                        id="name"
+                        name="name"
+                        type="text"
+                        defaultValue={productName}
+                        className="flex items-center h-12 px-4 mt-2 rounded text-gray-900 focus:outline-none focus:ring-2 focus:border-violet-400 focus:dark:border-violet-600 focus:ring-violet-400 focus:dark:ring-violet-600"
+                      />
+                      <label
+                        htmlFor="brand"
+                        className="self-start text-xs font-semibold"
+                      >
+                        Product Brand
+                      </label>
+                      <input
+                        id="brand"
+                        name="brand"
+                        type="text"
+                        defaultValue={brand}
+                        className="flex items-center h-12 px-4 mt-2 rounded text-gray-900 focus:outline-none focus:ring-2 focus:border-violet-400 focus:dark:border-violet-600 focus:ring-violet-400 focus:dark:ring-violet-600"
+                      />
+                      <label
+                        htmlFor="brand"
+                        className="self-start text-xs font-semibold"
+                      >
+                        Query Title
+                      </label>
+                      <input
+                        id="title"
+                        name="title"
+                        type="text"
+                        defaultValue={queryTitle}
+                        className="flex items-center h-12 px-4 mt-2 rounded text-gray-900 focus:outline-none focus:ring-2 focus:border-violet-400 focus:dark:border-violet-600 focus:ring-violet-400 focus:dark:ring-violet-600"
+                      />
+                      <label
+                        htmlFor="brand"
+                        className="self-start text-xs font-semibold"
+                      >
+                        Query Title
+                      </label>
+                      <input
+                        id="title"
+                        name="title"
+                        type="text"
+                        defaultValue={boycottReason}
+                        className="flex items-center h-20 px-4 mt-2 rounded text-gray-900 focus:outline-none focus:ring-2 focus:border-violet-400 focus:dark:border-violet-600 focus:ring-violet-400 focus:dark:ring-violet-600 textarea textarea-bordered textarea-md w-full max-w-xs"
+                      />
+                      <button
+                        type="submit"
+                        className="flex items-center justify-center h-12 px-6 mt-8 text-sm font-semibold rounded bg-violet-400 dark:bg-violet-600 text-gray-900 dark:text-gray-50"
+                      >
+                        Update
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+              <div className="modal-action">
+                <form method="dialog">
+                  <button className="btn btn-secondary">Close</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
         </div>
         <div className="flex items-center space-x-1">
           <button className="btn btn-warning">Delete</button>
