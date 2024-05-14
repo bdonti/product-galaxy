0e7c6e2a38b54b135a0ac4d5de04d372c62179ca
rahidhaque <ontirahid@yahoo.com>
2024-05-14 16:25:30 +0600

    Recommendations for me functionality implemented

--- src/pages/Recommendations/Recommendations.jsx
+++ src/pages/Recommendations/Recommendations.jsx
@@ -1,5 +1,51 @@
+import { useContext, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+
 const Recommendations = () => {
-  return <div></div>;
+  const { user } = useContext(AuthContext);
+  console.log(user.email);
+  const recommendations = useLoaderData();
+  const loadRecommendations = recommendations.filter(
+    (recommendation) => recommendation.userEmail === user.email
+  );
+  console.log(loadRecommendations);
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>My Query</th>
+              <th>Recommended Product</th>
+              <th>Recommended Product Image</th>
+              <th>Recommender Mail</th>
+            </tr>
+          </thead>
+          <tbody>
+            {loadRecommendations.map((recommendation) => (
+              <tr key={recommendation._id}>
+                <td>{recommendation.queryTitle}</td>
+                <td>{recommendation.recommendationName}</td>
+                <td>
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src={recommendation.recommendationUrl}
+                        alt="Recommended Product"
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td>{recommendation.recommenderUserEmail}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Recommendations;
