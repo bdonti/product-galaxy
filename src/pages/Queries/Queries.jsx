1df0f459b6a1fa9fe3663596b8b9300ad7d07271
rahidhaque <ontirahid@yahoo.com>
2024-05-14 16:47:07 +0600

    Search Funcitionality Added based on product name

--- src/pages/Queries/Queries.jsx
+++ src/pages/Queries/Queries.jsx
@@ -3,16 +3,35 @@ import Query from "../Query/Query";
 
 const Queries = () => {
   const [queries, setQueries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
   useEffect(() => {
     fetch("http://localhost:5000/recentQueriesNoLimit")
       .then((res) => res.json())
       .then((data) => setQueries(data));
   }, []);
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredQueries = queries.filter((query) =>
+    query.productName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
+      <div className="flex justify-center items-center">
+        <input
+          type="text"
+          placeholder="Search Product Name For Queries"
+          className="input input-bordered input-primary w-full max-w-xs"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center gap-6">
-        {queries.map((query) => (
+        {filteredQueries.map((query) => (
           <Query key={query._id} query={query}></Query>
         ))}
       </div>
